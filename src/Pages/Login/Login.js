92d4cc30fbbda16375715f6946d66d2ec8ca1270
RakibulHasan2 <rakibulhasan99445@gmail.com>
2023-03-25 14:51:42 +0600

    signup worked

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../Images/Kathā.png'
 const Login = () => {
-    const [error, setError] = useState(null)
     return (
         <div className='flex justify-center h-screen items-center bg-slate-800'>
             <div className='border rounded-3xl bg-slate-600 px-16 py-7'>
@@ -15,7 +14,6 @@ const Login = () => {
                         <div className='mb-3'>
                             <input className='outline-green-800 p-2 w-full rounded-lg' type="password" name='password' id='' placeholder='Your password' />
                         </div>
-                        <p className='text-error'>{error}</p>
                         <button className='btn w-full bg-green-600 border-none rounded-lg' >Login</button>
                     </form>
                 </div>
--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,9 +1,11 @@
+import axios from 'axios';
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
 import logo from '../../Images/Kathā.png'
 
 const SignUp = () => {
-    const [error, setError] = useState(null)
+    const navigate = useNavigate();
     const [values, setValues] = useState({
         username: "",
         email: "",
@@ -11,31 +13,68 @@ const SignUp = () => {
         confirmPassword: "",
     });
 
+    const toastOptions = {
+        position: "bottom-right",
+        autoClose: 1000,
+        pauseOnHover: true,
+        draggable: true,
+        theme: "dark",
+    };
+
+    const handleValidation = () => {
+        const { password, confirmPassword, username, email } = values;
+        if (password !== confirmPassword) {
+            console.log(password, confirmPassword)
+            toast.error(
+                "Password and confirm password should be same.",
+                toastOptions
+            );
+            return false;
+        } else if (username.length < 3) {
+            toast.error(
+                "Username should be greater than 3 characters.",
+                toastOptions
+            );
+            return false;
+        } else if (password.length < 8) {
+            toast.error(
+                "Password should be equal or greater than 8 characters.",
+                toastOptions
+            );
+            return false;
+        } else if (email === "") {
+            toast.error("Email is required.", toastOptions);
+            return false;
+        }
+        return true;
+    };
+
     const handleChange = (event) => {
         setValues({ ...values, [event.target.name]: event.target.value });
+        console.log()
     };
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-        // if (handleValidation()) {
-        //   const { email, username, password } = values;
-        //   const { data } = await axios.post(registerRoute, {
-        //     username,
-        //     email,
-        //     password,
-        //   });
+        if (handleValidation()) {
+            const { email, username, password } = values;
+            const { data } = await axios.post("http://localhost:5000/auth/register", {
+                username,
+                email,
+                password,
+            });
 
-        //   if (data.status === false) {
-        //     toast.error(data.msg, toastOptions);
-        //   }
-        //   if (data.status === true) {
-        //     localStorage.setItem(
-        //       process.env.REACT_APP_LOCALHOST_KEY,
-        //       JSON.stringify(data.user)
-        //     );
-        //     navigate("/");
-        //   }
-        // }
+            if (data.status === false) {
+                toast.error(data.msg, toastOptions);
+            }
+            if (data.status === true) {
+                localStorage.setItem(
+                    "chat-app-user",
+                    JSON.stringify(data.user)
+                );
+                navigate("/");
+            }
+        }
     };
     return (
         <div className='flex justify-center h-screen items-center bg-slate-800'>
@@ -44,23 +83,47 @@ const SignUp = () => {
                 <div className=''>
                     <form onSubmit={handleSubmit}>
                         <div className='mb-3'>
-                            <input className='outline-green-800 p-2 w-full rounded-lg ' type="text" name='name' id='' placeholder='Your Name' />
+                            <input
+                                className='outline-green-800 p-2 px-6 w-full rounded-lg '
+                                type="text"
+                                name='username'
+                                placeholder='User Name'
+                                onChange={(e) => handleChange(e)}
+                            />
                         </div>
                         <div className='mb-3'>
-                            <input className='outline-green-800 p-2 w-full rounded-lg' type="email" name='email' id='' placeholder='Your email address' />
+                            <input
+                                className='outline-green-800 p-2 px-6 w-full rounded-lg'
+                                type="email"
+                                name='email'
+                                placeholder='Your email address'
+                                onChange={(e) => handleChange(e)}
+                            />
                         </div>
                         <div className='mb-3'>
-                            <input className='outline-green-800 p-2 w-full rounded-lg' type="password" name='password' id='' placeholder='Your password' />
+                            <input
+                                className='outline-green-800 p-2 px-6 w-full rounded-lg'
+                                type="password"
+                                name='password'
+                                placeholder='Your password'
+                                onChange={(e) => handleChange(e)}
+                            />
                         </div>
                         <div className='mb-3'>
-                            <input className=' p-2 w-full outline-green-800 rounded-lg' type="password" name='Confirm' id='' placeholder='Confirm Your Password' />
+                            <input
+                                className='p-2 w-full outline-green-800 rounded-lg px-6'
+                                type="password"
+                                name='confirmPassword'
+                                placeholder='Confirm Your Password'
+                                onChange={(e) => handleChange(e)}
+                            />
                         </div>
-                        <p className='text-error'>{error}</p>
                         <button className='btn w-full bg-green-600 border-none rounded-lg' >Submit</button>
                     </form>
                 </div>
-                <p className='text-center mt-4 font-bold text-white'>Already Have an account? <Link className='' to='/login'>Login</Link></p>    
+                <p className='text-center mt-4 font-bold text-white'>Already Have an account? <Link className='' to='/login'>Login</Link></p>
             </div>
+            <ToastContainer></ToastContainer>
         </div>
     );
 };
